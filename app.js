3ae37ee92cd28b69bf7e1effefc0c87663d3e76a
Matti Luukkainen <mluukkai@iki.fi>
2022-09-16 15:18:31 +0300

    more shit

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const app = express()
 
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
+
 // Heroku dynamically sets a port
 const PORT = process.env.PORT || 5000
 
@@ -8,5 +12,5 @@ app.use(express.static('dist'))
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
-  console.log('server started on port 5000')
+  console.log('server started on port', PORT)
 })
