be842bcbd8b9b1a8a2d50889055252cd3e5fbf1d
Matti Luukkainen <mluukkai@iki.fi>
2022-09-16 19:47:43 +0300

    v3

--- app.js
+++ app.js
@@ -2,14 +2,13 @@ const express = require('express')
 const app = express()
 
 app.get('/health', (req, res) => {
-  const x = 1 / 0
-  // eslint-disable-next-line no-console
-  console.log(x)
+  throw 'error...'
+  // eslint-disable-next-line no-unreachable
   res.send('ok')
 })
 
 app.get('/version', (req, res) => {
-  res.send('2')
+  res.send('3')
 })
 
 // Heroku dynamically sets a port
