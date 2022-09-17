070a970ca8b2a6c476982dd3c47be6816c4a16fb
Matti Luukkainen <mluukkai@iki.fi>
2022-09-17 16:23:07 +0300

    cleaning up script

--- health_check.sh
+++ health_check.sh
@@ -1,20 +1,11 @@
 #!/bin/bash
 
-echo "Hello from shell script"
 
 OUT=$(curl localhost:8080/health)
 
-echo $?
-echo '-'
-echo $OUT
-echo '-'
-echo $OUT == 'ok' 
-
 if [ $OUT == 'ok' ]; 
 then
-    echo 'equal'
     exit 0
 else 
-    echo 'not equal'
     exit 1
 fi
